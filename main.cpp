8a8b7053ce300ef2385f675615fa7acea5460536
Rajesh Pandian M <mrpraj@gmail.com>
2022-09-30 14:40:08 +0530

    dev push

--- main.cpp
+++ main.cpp
@@ -2,16 +2,13 @@
 //~~~Author:Rajesh Pandian M | mrprajesh.co.in
 #include <bits/stdc++.h>
 using namespace std;
-class A{
+class A {
   int val;
-  
-  public:
-    A(int v): 
-      val(v){}
-    
-    void print(){
-      std::cout<< "val:" << val << '\n';
-    }
+
+    public:
+  A(int v) : val(v) {}
+
+  void print() { std::cout << "val:" << val << '\n'; }
 };
 int main(int argc, char* argv[]) {
   std::cout << "Hello Dear Programmer! \n\n --cpp\n";
@@ -19,22 +16,22 @@ int main(int argc, char* argv[]) {
   //~ size_t N = 1024;
   //~ size_t ii = 0;
   //~ try {
-    //~ int* a = (int*) malloc (sizeof(int) * N);
-    
-    //~ for (ii = 0; ii < 100*N; ++ii) {
-      //~ a[ii] = ii;
-    //~ }
+  //~ int* a = (int*) malloc (sizeof(int) * N);
+
+  //~ for (ii = 0; ii < 100*N; ++ii) {
+  //~ a[ii] = ii;
+  //~ }
   //~ } catch (...) {
-    //~ std::cout << "Exception! ";
+  //~ std::cout << "Exception! ";
   //~ }
 
   //~ std::cout << "Count:" << ii << '\n';
 
   A* a = new A(10);
   a->print();
-  
-  A* b;
-  b->print();
-  
+
+  // A* b;
+  // b->print();
+
   return 0;
 }
